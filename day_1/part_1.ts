265cdf3e10e8e2f83cea6d9d8f83436cf912a1f1
Yakiyo <91675932+Yakiyo@users.noreply.github.com>
2022-12-02 15:00:35 +0600

    feat: finish day 1

---
+++ day_1/part_1.ts
@@ -0,0 +1,14 @@
+const str = Deno.readTextFileSync(".calorie.txt").split("\n\r");
+const val: number[] = [];
+
+str.forEach((e) => {
+  const v = e.split("\r\n");
+  const c = v.map((x) => {
+    x.replaceAll("\r", "").replaceAll("\n", "");
+    return Number(x);
+  }).reduce((a, b) => a + b, 0);
+  val.push(c);
+});
+val.sort((a, b) => a > b ? -1 : 1);
+
+console.log(val[0]);
