267f5b65c553b5a5b938cbfadd78994db27735e1
Yakiyo <91675932+Yakiyo@users.noreply.github.com>
2022-12-04 14:15:53 +0600

    feat: setup
    - run scripts more easily now

---
+++ mod.ts
@@ -0,0 +1,32 @@
+export function exists(p: string): boolean {
+    try {
+      Deno.statSync(p);
+      return true;
+    } catch (_) {
+      return false;
+    }
+}
+
+  
+if (Deno.args.length < 2) {
+  console.error("Please pass both day number and part number argument");
+  Deno.exit(1);
+}
+
+const [day, part] = Deno.args;
+
+if (isNaN(Number(day)) || isNaN(Number(part))) {
+  console.error("Arguments day and part must both be whole integers");
+  Deno.exit(1);
+}
+
+const path = `./day_${day}/part_${part}.ts`;
+
+if (!exists(path)) {
+    console.error("Invalid path. Does not exist");
+    Deno.exit(1);
+}
+
+await import(path);
+
+Deno.exit(0);
