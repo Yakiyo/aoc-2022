97580b5735cb245938a5ec3b6ee40ba13a5ede5f
Yakiyo <91675932+Yakiyo@users.noreply.github.com>
2022-12-04 13:59:08 +0600

    feat: finish day 4

---
+++ day_4/part_2.ts
@@ -0,0 +1,21 @@
+const values = Deno.readTextFileSync("./inputs/day_4.txt").split("\r\n");
+
+let count = 0;
+
+for (let i = 0; i < values.length; i++) {
+  const [p1, p2] = values[i].split(",");
+  const q = p1.split("-");
+  const r = p2.split("-");
+
+  if (overlaps(q, r)) count++;
+}
+
+console.log(count);
+
+function overlaps(q: string[], r: string[]) {
+  const s = q.map((x) => Number(x));
+  const p = r.map((x) => Number(x));
+  if (s[0] < p[0] && s[1] < p[0]) return false;
+  if (s[0] > p[1] && s[1] > p[1]) return false;
+  return true;
+}
