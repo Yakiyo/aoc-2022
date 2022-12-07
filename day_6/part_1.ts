ba8d3945beb9fbbd415a418508dd0bdc028df459
Yakiyo <91675932+Yakiyo@users.noreply.github.com>
2022-12-07 12:13:26 +0600

    chore: deno fmt

--- day_6/part_1.ts
+++ day_6/part_1.ts
@@ -1,11 +1,10 @@
-const values = Deno.readTextFileSync('./inputs/day_6.txt').split("");
-
+const values = Deno.readTextFileSync('./inputs/day_6.txt').split('');
 
 for (let i = 0; i < values.length; i++) {
 	const c = new Set(values.slice(i, i + 4));
-    if (Array.from(c).length === 4) {
+	if (Array.from(c).length === 4) {
 		console.log(c);
-        console.log(i + 4);
+		console.log(i + 4);
 		i = values.length;
 	}
 }
--- day_6/part_2.ts
+++ day_6/part_2.ts
@@ -1,11 +1,10 @@
-const values = Deno.readTextFileSync('./inputs/day_6.txt').split("");
-
+const values = Deno.readTextFileSync('./inputs/day_6.txt').split('');
 
 for (let i = 0; i < values.length; i++) {
 	const c = new Set(values.slice(i, i + 14));
-    if (Array.from(c).length === 14) {
+	if (Array.from(c).length === 14) {
 		console.log(c);
-        console.log(i + 14);
+		console.log(i + 14);
 		i = values.length;
 	}
 }
