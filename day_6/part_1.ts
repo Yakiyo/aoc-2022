4f8168cf8d6fc80e4bf4c12643462f4463aa2458
Yakiyo <91675932+Yakiyo@users.noreply.github.com>
2022-12-07 12:04:21 +0600

    feat: finish day 6

---
+++ day_6/part_1.ts
@@ -0,0 +1,11 @@
+const values = Deno.readTextFileSync('./inputs/day_6.txt').split("");
+
+
+for (let i = 0; i < values.length; i++) {
+	const c = new Set(values.slice(i, i + 4));
+    if (Array.from(c).length === 4) {
+		console.log(c);
+        console.log(i + 4);
+		i = values.length;
+	}
+}
